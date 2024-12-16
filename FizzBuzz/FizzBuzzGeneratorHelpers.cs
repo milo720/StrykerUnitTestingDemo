fcf5ce49a1631a2ad2f1f100157ef7545ae06b8d
milo farrell <milofarrell@microsoft.com>
2024-12-16 15:33:35 +0000

    New start without cosntants.

--- FizzBuzz/FizzBuzzGeneratorHelpers.cs
+++
@@ -1,6 +0,0 @@
-﻿internal static class FizzBuzzGeneratorHelpers
-{
-    public const string FizzBuzz = "FizzBuzzOrg";
-    public const string Buzz = "Buzz";
-    public const string Fizz = "Fizz";
-}
